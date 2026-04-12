09e85e71d82bd38ea8ccd47e6f020cccdb71f3a0
GMshuS <303150304@163.com>
2026-04-12 12:46:53 +0800

    优化目录结构

--- VC/vs2010_run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "C:\CommonDev\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x64
-echo VS2010 x64 编译环境已就绪
-echo 可使用 cl.exe nmake.exe link.exe lib.exe
-cmd /k
--- VC/vs2022_run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: 把下面路径改成你自己的 vcvars64.bat 路径
-call "C:\CommonDev\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-echo ========== VS 编译环境已启用 ==========
-echo 可直接使用：cl.exe  nmake.exe  lib.exe  link.exe
-cd /d %userprofile%\desktop
-cmd /k
